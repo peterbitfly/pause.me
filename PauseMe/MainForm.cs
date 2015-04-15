430b552017c048e5dbed0c3216b9ae98dbdc6b70
Peter <pratscher@voezfi.at>
2015-04-15 13:18:58 +0200

    License added

--- PauseMe/MainForm.cs
+++ PauseMe/MainForm.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <peter@pratscher.org> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
