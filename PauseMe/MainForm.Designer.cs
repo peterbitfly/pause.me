22d71cf4de8cceccad9aed3d420d810242fa0a76
Peter <pratscher@voezfi.at>
2015-04-15 13:07:44 +0200

    Initial version

---
+++ PauseMe/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PauseMe
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tmrCountdown = new System.Windows.Forms.Timer(this.components);
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.niMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tbxStatus = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.tmrUpdateStatus = new System.Windows.Forms.Timer(this.components);
+            this.cmsMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrCountdown
+            // 
+            this.tmrCountdown.Interval = 1000;
+            this.tmrCountdown.Tick += new System.EventHandler(this.tmrCountdown_Tick);
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCountdown.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountdown.Location = new System.Drawing.Point(0, 0);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(924, 366);
+            this.lblCountdown.TabIndex = 0;
+            this.lblCountdown.Text = "Countdown Timer";
+            this.lblCountdown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCountdown.Click += new System.EventHandler(this.lblCountdown_Click);
+            this.lblCountdown.DoubleClick += new System.EventHandler(this.lblCountdown_DoubleClick);
+            // 
+            // niMain
+            // 
+            this.niMain.ContextMenuStrip = this.cmsMain;
+            this.niMain.Icon = ((System.Drawing.Icon)(resources.GetObject("niMain.Icon")));
+            this.niMain.Text = "Pause Me";
+            this.niMain.Visible = true;
+            // 
+            // cmsMain
+            // 
+            this.cmsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbxStatus,
+            this.toolStripSeparator2,
+            this.startToolStripMenuItem,
+            this.pauseToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.cmsMain.Name = "cmsMain";
+            this.cmsMain.Size = new System.Drawing.Size(161, 105);
+            // 
+            // tbxStatus
+            // 
+            this.tbxStatus.Enabled = false;
+            this.tbxStatus.Name = "tbxStatus";
+            this.tbxStatus.Size = new System.Drawing.Size(100, 21);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(157, 6);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.pauseToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(157, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Interval = 1200000;
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // tmrUpdateStatus
+            // 
+            this.tmrUpdateStatus.Interval = 1000;
+            this.tmrUpdateStatus.Tick += new System.EventHandler(this.tmrUpdateStatus_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 366);
+            this.Controls.Add(this.lblCountdown);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.cmsMain.ResumeLayout(false);
+            this.cmsMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrCountdown;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.NotifyIcon niMain;
+        private System.Windows.Forms.ContextMenuStrip cmsMain;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox tbxStatus;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.Timer tmrUpdateStatus;
+    }
+}
+
---
+++ PauseMe/MainForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PauseMe
+{
+    public partial class MainForm : Form
+    {
+        private int _CountDownTimer = 0;
+
+        private DateTime _TimerStarted;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.TopMost = true;
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Opacity = 0.5;
+            this.WindowState = FormWindowState.Maximized;
+
+            lblCountdown.Text = "";
+            tbxStatus.Text = "Stopped";
+        }
+
+        private void tmrCountdown_Tick(object sender, EventArgs e)
+        {
+            lblCountdown.Text = "Pause time: " + (20 - _CountDownTimer++) + "s";
+
+            if (_CountDownTimer == 21)
+            {
+                _CountDownTimer = 0;
+                tmrCountdown.Stop();
+                this.Hide();
+            }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tmrMain.Start();
+            _TimerStarted = DateTime.Now;
+            tmrUpdateStatus.Start();
+
+            tbxStatus.Text = "Running";
+        }
+
+        private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tmrMain.Stop();
+            tmrUpdateStatus.Stop();
+            tbxStatus.Text = "Stopped";
+            niMain.Text = "Pause Me - Stopped";
+        }
+
+        private void lblCountdown_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            BeginInvoke(new MethodInvoker(delegate
+            {
+                Hide();
+                startToolStripMenuItem_Click(this, null);
+                niMain.ShowBalloonTip(5000, "Pause Me Started", "Pause Me has been started an will gently remind you every 20 minutes to rest your eyes!", ToolTipIcon.Info);
+            }));
+        }
+
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            lblCountdown.Text = "Pause time: " + (20 - _CountDownTimer++) + "s";
+            this.Show();
+            _TimerStarted = DateTime.Now;
+            tmrCountdown.Start();
+        }
+
+        private void lblCountdown_DoubleClick(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void tmrUpdateStatus_Tick(object sender, EventArgs e)
+        {
+            var timePassed = (DateTime.Now - (_TimerStarted.AddMinutes(20))).Negate();
+            var minutesPassed = timePassed.Minutes.ToString("00");
+            var secondsPassed = timePassed.Seconds.ToString("00");
+
+            tbxStatus.Text = "Running (" + minutesPassed + ":" + secondsPassed + ")";
+            niMain.Text = "Pause Me - Running (" + minutesPassed + ":" + secondsPassed + ")";
+        }
+    }
+}
